358ea33f00d14add1d963838b8a3a5f624fb8a55
Yulia <linguavoice1@yahoo.com>
2024-12-29 18:17:43 +0300

    get identity matrix implemented

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -418,8 +418,18 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Function not implemented');
+
+// identity matrix is a square matrix. where all lements are zeros
+// besides the principal diagonal (where all elements equal to 1);
+// number of rows === number of columns;
+function getIdentityMatrix(n) {
+  const matrix = Array.from({ length: n }).map(() =>
+    Array.from({ length: n }).fill(0)
+  );
+
+  return matrix.map((item, index) =>
+    item.map((el, i) => (i === index ? 1 : 0))
+  );
 }
 
 /**
